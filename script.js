fcec21ef589be36708a4fa361e1bbe83b5c4a740
pauly-johnson <paul.m.johnson1988@gmail.com>
2025-01-22 10:53:48 +1300

    building rules

--- script.js
+++ script.js
@@ -2,15 +2,17 @@ const startButton = document.getElementById("start");
 const operatorDisplay = document.querySelector(".operators");
 const questionDisplay = document.querySelector(".questions");
 const quizDisplay = document.querySelector(".quiz");
-const questionsCompleted = document.getElementById("questionsCompleted");
+const resultsDisplay = document.querySelector(".resultsDisplay");
+const questionsCorrect = document.getElementById("questionsCompleted");
 const questionsTotal = document.getElementById("questionsTotal");
-const messageDisplay = document.getElementById('messageDisplay')
+const messageDisplay = document.getElementById("messageDisplay");
 
 let operator = "";
 let pram1 = 0;
 let pram2 = 0;
 let result = 0;
 let numberOfQuestions = 0;
+let questionsAttempted = 0;
 
 document.querySelectorAll(".operator-btn").forEach((button) => {
   button.addEventListener("click", () => {
@@ -28,35 +30,72 @@ document.getElementById("start").addEventListener("click", () => {
       document.getElementById("questionsNum").value
     );
     questionsTotal.innerHTML = numberOfQuestions;
-    questionsCompleted.innerHTML = 0;
+    questionsCorrect.innerHTML = 0;
 
     generateQuestion();
   }
 });
 
 function generateQuestion() {
-  pram1 = Math.floor(Math.random() * 100);
-  pram2 = Math.floor(Math.random() * 100);
-  
-  document.querySelector(
-    ".question"
-  ).innerHTML = ` What is: ${pram1} ${operator} ${pram2} = ?`;
+  if (questionsAttempted === numberOfQuestions) {
+    // alert("Quiz Completed");
+    quizDisplay.style.display = "none";
+    resultsDisplay.style.display = "block";
+    document.getElementById("correct").innerHTML = questionsCorrect.innerHTML;
+    document.getElementById("incorrect").innerHTML =
+      numberOfQuestions - parseFloat(questionsCorrect.innerHTML);
+    document.getElementById("accuracy").innerHTML = `${(
+      (parseFloat(questionsCorrect.innerHTML) / numberOfQuestions) *
+      100
+    ).toFixed(1)}%`;
+  } else {
+    pram1 = Math.floor(Math.random() * 100);
+    pram2 = Math.floor(Math.random() * 100);
+
+    if (operator === "/") {
+      while (
+        pram1 % pram2 !== 0 ||
+        pram1 === 0 ||
+        pram2 === 0 ||
+        pram1 < pram2
+      ) {
+        pram1 = Math.floor(Math.random() * 100);
+        pram2 = Math.floor(Math.random() * 100);
+      }
+    } else if (operator === "-") {
+      while (pram1 < pram2 || pram1 === 0 || pram2 === 0) {
+        pram1 = Math.floor(Math.random() * 100);
+        pram2 = Math.floor(Math.random() * 100);
+      }
+    } else if (operator === "*") {
+      while (pram1 === 0 || pram2 === 0 || pram1 > 14) {
+        pram1 = Math.floor(Math.random() * 100);
+        pram2 = Math.floor(Math.random() * 100);
+      }
+    } else if (operator === "+") {
+      while (pram1 === 0 || pram2 === 0) {
+        pram1 = Math.floor(Math.random() * 100);
+        pram2 = Math.floor(Math.random() * 100);
+      }
+    }
+    document.querySelector(
+      ".question"
+    ).innerHTML = ` What is: ${pram1} ${operator} ${pram2} = ?`;
+  }
 }
 
-document.getElementById('next').addEventListener('click', () => {
-  console.log(pram1, pram2);
-  
+document.getElementById("next").addEventListener("click", () => {
   checkResult();
-  if (result === parseFloat(document.getElementById('userAnswer').value)) {
-  console.log(result);
-  // alert('Correct!');
-  questionsCompleted.innerHTML = parseFloat(questionsCompleted.innerHTML) + 1;
-  messageDisplay.innerHTML = 'Correct !!! You are doing great';
-  generateQuestion();
-} else {
-  // alert('Incorrect!');
-  messageDisplay.innerHTML = 'Incorrect !!! Good try';
-}
+  if (result === parseFloat(document.getElementById("userAnswer").value)) {
+    questionsCorrect.innerHTML = parseFloat(questionsCorrect.innerHTML) + 1;
+    messageDisplay.innerHTML = "Correct !!! Great work";
+    questionsAttempted++;
+    generateQuestion();
+  } else {
+    messageDisplay.innerHTML = "Incorrect !!! Good try";
+    questionsAttempted++;
+    generateQuestion();
+  }
 });
 
 function checkResult() {
@@ -70,4 +109,3 @@ function checkResult() {
     result = pram1 / pram2;
   }
 }
-
